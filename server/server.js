c5e303f20451cf010fb622c575ef3bc0428e438a
Ashley Wessel <amerino.dance@gmail.com>
2023-12-14 15:09:04 -0600

    added searchBar router

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const favoriteRouter = require('./routes/favorite.router');
 const categoryRouter = require('./routes/category.router');
+const searchBarRouter = require('./routes/searchBar.router');
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -10,6 +11,7 @@ app.use(express.static('build'));
 
 app.use('/api/favorites', favoriteRouter);
 app.use('/api/categories', categoryRouter);
+app.use('/api/searchBar', searchBarRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
